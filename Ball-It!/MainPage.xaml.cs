07e3d96e3b9b2cd91a617649d1b3c666cf3ea755
Evan Simkowitz <Evan.Simkowitz@studentpartner.com>
2016-03-18 02:18:22 -0400

    new pull

---
+++ Ball-It!/MainPage.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.ApplicationModel;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Ball_It
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SpheroManager sp;
+        int lastHeading = 0;
+        public MainPage()
+        {
+            sp = new SpheroManager();
+            DataContext = sp;
+
+            this.InitializeComponent();
+
+            CoreWindow.GetForCurrentThread().KeyDown += MainPage_KeyDown;
+            CoreWindow.GetForCurrentThread().KeyUp += MainPage_KeyUp;
+        }
+
+        /// <summary> 
+        /// Invoked when this page is about to be displayed in a Frame. 
+        /// </summary> 
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter 
+        /// property is typically used to configure the page.</param> 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Application app = Application.Current;
+            app.Suspending += OnSuspending;
+        }
+
+        //! @brief  handle the application entering the background 
+        private void OnSuspending(object sender, SuspendingEventArgs args)
+        {
+            sp.ShutdownRobotConnection();
+            ConnectionToggle.IsOn = false;
+        }
+
+        /*! 
+          * @brief   handle the user launching this page in the application 
+          *  
+          *  connects to sphero and sets up the ui 
+          */
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            sp.ShutdownRobotConnection();
+            Application app = Application.Current;
+            app.Suspending -= OnSuspending;
+            ConnectionToggle.IsOn = false;
+        }
+
+
+        private void MainPage_KeyUp(CoreWindow sender, KeyEventArgs args)
+        {
+            if (sp.m_robot != null)
+            {
+
+                sp.m_robot.Roll(lastHeading, 0f);
+            }
+        }
+
+        private void MainPage_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            if (sp.m_robot != null)
+            {
+                switch (args.VirtualKey)
+                {
+                    case Windows.System.VirtualKey.Up:
+                        //roll forward
+                        sp.m_robot.Roll(0, (float)1);
+                        lastHeading = 0;
+                        break;
+                    case Windows.System.VirtualKey.Down:
+                        sp.m_robot.Roll(180, (float)1);
+                        lastHeading = 180;
+                        break;
+                    case Windows.System.VirtualKey.Left:
+                        sp.m_robot.Roll(270, (float)1);
+                        lastHeading = 270;
+                        break;
+                    case Windows.System.VirtualKey.Right:
+                        sp.m_robot.Roll(90, (float)1);
+                        lastHeading = 90;
+                        break;
+                }
+            }
+        }
+        private void ConnectionToggle_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (ConnectionToggle.IsOn)
+            {
+                if (sp.m_robot == null)
+                {
+                    ConnectionToggle.OnContent = "Connecting...";
+                    sp.SetupRobotConnection();
+                    grdControls.Visibility = Visibility.Visible;
+
+                }
+            }
+            else
+            {
+                sp.ShutdownRobotConnection();
+                ConnectionToggle.OffContent = "Disconnected";
+                grdControls.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
---
+++ Ball-It!/SpheroManager.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Popups;
+using RobotKit;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml.Navigation;
+
+namespace Ball_It
+{
+    class SpheroManager : INotifyPropertyChanged
+    {
+
+        public Sphero m_robot = null;
+
+        private string SpheroName_;
+        public string SpheroName
+        {
+            get { return SpheroName_; }
+            set
+            {
+                SpheroName_ = value;
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("SpheroName"));
+                }
+            }
+        }
+
+        private const string kNoSpheroConnected = "No Sphero Connected";
+
+        //! @brief  the default string to show when connecting to a sphero ({0})
+        private const string kConnectingToSphero = "Connecting to {0}";
+
+        //! @brief  the default string to show when connected to a sphero ({0})
+        private const string kSpheroConnected = "Connected to {0}";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SpheroManager()
+        {
+            //SetupRobotConnection();
+        }
+
+        public void SetupRobotConnection()
+        {
+            RobotProvider provider = RobotProvider.GetSharedProvider();
+            provider.DiscoveredRobotEvent += OnRobotDiscovered;
+            provider.NoRobotsEvent += OnNoRobotsEvent;
+            provider.ConnectedRobotEvent += OnRobotConnected;
+            provider.FindRobots();
+        }
+
+        //! @brief  when a robot is discovered, connect! 
+        private void OnRobotDiscovered(object sender, Robot robot)
+        {
+            Debug.WriteLine(string.Format("Discovered \"{0}\"", robot.BluetoothName));
+
+            if (m_robot == null)
+            {
+                RobotProvider provider = RobotProvider.GetSharedProvider();
+                provider.ConnectRobot(robot);
+                m_robot = (Sphero)robot;
+            }
+        }
+
+        private bool SpheroConnected_ = false;
+        public bool SpheroConnected
+        {
+            get { return SpheroConnected_; }
+            set
+            {
+                SpheroConnected_ = value;
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("SpheroConnected"));
+                }
+            }
+        }
+
+        private void OnNoRobotsEvent(object sender, EventArgs e)
+        {
+            MessageDialog dialog = new MessageDialog(kNoSpheroConnected);
+        }
+
+        //! @brief  when a robot is connected, get ready to drive!
+        private void OnRobotConnected(object sender, Robot robot)
+        {
+            Debug.WriteLine(string.Format(kSpheroConnected, robot));
+            //ConnectionToggle.IsOn = true;
+            //ConnectionToggle.OnContent = "Connected";
+            SpheroConnected = true;
+            m_robot.SetRGBLED(255, 255, 255);
+            SpheroName = string.Format(kSpheroConnected, robot.BluetoothName);
+
+            m_robot.SensorControl.Hz = 10;
+            m_robot.SensorControl.GyrometerUpdatedEvent += OnGyrometerUpdated;
+            m_robot.SensorControl.AccelerometerUpdatedEvent += OnAccelerometerUpdated;
+            //m_robot.CollisionControl.StartDetectionForWallCollisions();
+            //m_robot.CollisionControl.CollisionDetectedEvent += OnCollisionDetected;
+        }
+
+        private void OnAccelerometerUpdated(object sender, AccelerometerReading e)
+        {
+            AccelReading = string.Format("X:{0}" + Environment.NewLine + "Y:{1}" + Environment.NewLine + "Z:{2}", e.X, e.Y, e.Z);
+        }
+
+        private string _AccelReading;
+        public string AccelReading
+        {
+            get
+            {
+                return _AccelReading;
+            }
+            set
+            {
+                _AccelReading = value;
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("AccelReading"));
+                }
+            }
+        }
+
+        private void OnGyrometerUpdated(object sender, GyrometerReading e)
+        {
+            GyroReading = string.Format("X:{0}" + Environment.NewLine + "Y:{1}" + Environment.NewLine + "Z:{2}", e.X, e.Y, e.Z);
+        }
+
+        private string _GyroReading;
+        public string GyroReading
+        {
+            get
+            {
+                return _GyroReading;
+            }
+            set
+            {
+                _GyroReading = value;
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("GyroReading"));
+                }
+            }
+        }
+
+        public void ShutdownRobotConnection()
+        {
+            if (m_robot != null)
+            {
+                try
+                {
+                    m_robot.SensorControl.StopAll();
+                    m_robot.Sleep();
+                    m_robot.Disconnect();
+
+                    m_robot.SensorControl.AccelerometerUpdatedEvent -= OnAccelerometerUpdated;
+                    m_robot.SensorControl.GyrometerUpdatedEvent -= OnGyrometerUpdated;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                RobotProvider provider = RobotProvider.GetSharedProvider();
+                provider.DiscoveredRobotEvent -= OnRobotDiscovered;
+                provider.NoRobotsEvent -= OnNoRobotsEvent;
+                provider.ConnectedRobotEvent -= OnRobotConnected;
+            }
+        }
+    }
+}
